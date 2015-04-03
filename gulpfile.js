3d84526cfe5f43a675b6632a3be35f1eb4f4a769
Szerémi Attila <amcsi@mailbox.hu>
2015-04-03 17:07:47 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'); 
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');

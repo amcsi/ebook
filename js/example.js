1a142a1111ff8b0d5943749e3b9494a4b04f7b5b
Szerémi Attila <amcsi@mailbox.hu>
2015-04-10 21:39:13 +0100

    Display dummy chapter list

--- js/example.js
+++ js/example.js
@@ -1,3 +1,87 @@
-var lol = <div>lol</div>;
+//var SideChapterList = require("SideChapterList");
 
-document.body.innerHTML = 'hello world!';
+//require('./RecursiveSideChapters');
+
+var RecursiveSideChapters = React.createClass({
+    
+    getIndent: function() {
+        var i, length;
+        var ret = '';
+        for (i = 0, length = this.props.level; i < length; i++) {
+            ret += '----';
+        }
+        ret += '|-';
+
+        return ret;
+    },
+
+    render: function () {
+        var indent = this.getIndent();
+        var i, length, singleChapterData, parts;
+
+        var items = [];
+        var nextLevel = parseInt(this.props.level, 10) + 1;
+
+        for (i = 0, length = this.props.chapterData.length; i < length; i++) {
+            singleChapterData = this.props.chapterData[i];
+            var children = '';
+            if (singleChapterData.children && singleChapterData.children.length) {
+                children = <RecursiveSideChapters chapterData={singleChapterData.children} level={nextLevel} />;
+            }
+            items.push(
+                <div key={i}>
+                    <h4>{indent} {singleChapterData.name}</h4>
+                    {children}
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                {items}
+            </div>
+        );
+    }
+});
+
+var SideChapterList = React.createClass({
+    getInitialState: function() {
+        return {
+            chapterData: [
+                {
+                    key: 0,
+                    name: 'Chapter 1',
+                    children: [
+                        {
+                            key: 1,
+                            name: 'Chapter 1/A'
+                        },
+                        {
+                            key: 2,
+                            name: 'Chapter 2/A',
+                            children: [
+                                {
+                                    key: 3,
+                                    name: 'Chapter 2/A/I'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    key: 4,
+                    name: 'Chapter 2'
+                }
+            ]
+        };
+    },
+    render: function() {
+        return (
+            <div>
+                <RecursiveSideChapters chapterData={this.state.chapterData} level="1" />
+            </div>
+        );
+    }
+});
+
+React.render(<SideChapterList />, document.getElementById('chaptersCol'));
